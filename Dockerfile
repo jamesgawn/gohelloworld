53da024e71e13b369c1a6dad53bb6ba8fb451e18
akyan <james@gawn.uk>
2018-11-25 17:20:04 +0000

    Initial commit of hello world web server, with Dockerfile to build containerised version.

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM golang:alpine as build
+WORKDIR $GOPATH/src/github.com/jamesgawn/gohelloworld
+
+# Copy the rest of the project and build
+COPY . .
+RUN CGO_ENABLED=0 GOOS=linux go build -a -o /helloworld .
+
+# Reset to scratch to drop all of the above layers and only copy over the final binary
+FROM scratch
+ENV HOME=/home
+COPY --from=build /helloworld /bin/helloworld
+
+ENTRYPOINT ["/bin/helloworld"]
+
+EXPOSE 8080
---
+++ hello.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func main() {
+	host, err := os.Hostname()
+
+	if err != nil {
+		panic(err)
+	}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Hello world!")
+		fmt.Fprintln(w, "Host: ", host)
+		fmt.Fprintln(w, "User-Agent: ", r.UserAgent())
+		fmt.Fprintln(w, "Source Host: ", r.Host)
+		fmt.Fprintln(w, "Path: ", r.URL.Path)
+	})
+
+	http.ListenAndServe(":8080", nil)
+}
